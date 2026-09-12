0479e0acb2f8b5c9fe1ff93d5c6227e379d9c2c4
pineromaita-coder <pineromaita@gmail.com>
2026-09-12 04:21:13 -0300

    Criar sidepanel.js

---
+++ sidepanel.js
@@ -0,0 +1,13 @@
+const side=document.createElement("div");
+
+side.id="sidepanel";
+
+side.innerHTML=`
+<div>⚡ NovaCore</div>
+<div>📡 Radar</div>
+<div>💰 Mercado</div>
+<div>🤖 IA</div>
+<div>⚙️ Sistema</div>
+`;
+
+document.body.appendChild(side);
